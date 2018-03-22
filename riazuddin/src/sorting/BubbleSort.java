3d6b07fb923f4301865e41f541f6e67c40ace51c
Riaz Uddin <ruuddin@yahoo.com>
2018-03-22 12:13:25 -0700

    minor fix

--- riazuddin/src/sorting/BubbleSort.java
+++ riazuddin/src/sorting/BubbleSort.java
@@ -15,7 +15,7 @@ public class BubbleSort {
 	
 	private static void bubbleSort(int[] a) {
 		for (int i = 0; i < a.length; i++) {
-			for(int j = 0; j < a.length - i - 1; j++) {
+			for(int j = 0; j < a.length - 1; j++) {
 				if(a[j] > a[j+1]) {
 					int t = a[j];
 					a[j] = a[j+1];
--- riazuddin/src/sorting/QuickSort.java
+++ riazuddin/src/sorting/QuickSort.java
@@ -11,6 +11,7 @@ public class QuickSort {
     }
 
 	private static void printArray(int[] A) {
+		System.out.print("\t\t");
 		for (int i = 0; i < A.length; i++){
             System.out.print(A[i] + " ");
         }
@@ -19,6 +20,7 @@ public class QuickSort {
 
     private static void QuickSort(int[] a, int l, int r) {
         int i;
+        System.out.println("l: "+l+" r: "+r);
         if (r > l){
             i = partition(a, l, r);
             QuickSort(a, l, i-1);
@@ -27,6 +29,7 @@ public class QuickSort {
     }
 
     private static int partition(int[] a, int l, int r) {
+    		System.out.println("\tpartition");
         int v = a[r];
         int i = l;
         int j = r;
@@ -39,6 +42,7 @@ public class QuickSort {
                 j = j - 1;
             }
             temp = a[i];
+            System.out.println("i: "+i+" j: "+j);
             if (i < j){
                 a[i] = a[j];
                 a[j] = temp;
