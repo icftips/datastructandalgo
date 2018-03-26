3dcd2074b7c8ea5a351f2a7fb4a8d53e063684fd
Riaz Uddin <ruuddin@yahoo.com>
2018-03-26 11:26:54 -0700

    bug fix

--- riazuddin/src/string/LongestCommonSubsequenceLengthRecursion.java
+++ riazuddin/src/string/LongestCommonSubsequenceLengthRecursion.java
@@ -8,16 +8,24 @@ public class LongestCommonSubsequenceLengthRecursion {
 	public static void main(String[] args) {
 		String s1 = "ABCDGH", s2 = "AEDFHR";
 
-		System.out.println(subsequenceLength(s1, s2, s1.length() - 1, s2.length() - 1));
+		System.out.println(subsequenceLength(s1, s2, s1.length() - 1, s2.length() - 1, 0));
 	}
 
-	private static int subsequenceLength(String s1, String s2, int i, int j) {
-		if (i == 0 || j == 0)
+	private static int subsequenceLength(String s1, String s2, int i, int j, int tab) {
+//		printSpace(tab);
+//		System.out.println(i+"("+s1.charAt(i)+"), "+j+"("+s2.charAt(j)+")");
+		if (i < 0 || j < 0)
 			return 0;
 
 		if (s1.charAt(i) == s2.charAt(j))
-			return 1 + subsequenceLength(s1, s2, i - 1, j - 1);
+			return 1 + subsequenceLength(s1, s2, i - 1, j - 1, tab+1);
 
-		return Math.max(subsequenceLength(s1, s2, i, j - 1), subsequenceLength(s1, s2, i - 1, j));
+		return Math.max(subsequenceLength(s1, s2, i, j - 1, tab+1), subsequenceLength(s1, s2, i - 1, j, tab+1));
+	}
+
+	private static void printSpace(int tab) {
+		for (int k = 0; k < tab; k++) {
+			System.out.print(" ");
+		}
 	}
 }
